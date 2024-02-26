193dee1e313b12405a16b784bcd484195526995e
Brandon H <brandonahimes@gmail.com>
2024-02-25 22:05:20 -0700

    update 1.2.x

---
+++ LiberartisPatch.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.ObjectSystem;
+
+namespace CalradiaExpanded
+{
+    [HarmonyPatch(typeof(DefaultMapDistanceModel), "GetDistance", new[] { typeof(MobileParty), typeof(Settlement) })]
+    internal class PartyToLiberartisPatch
+    {
+        public static void Postfix(MobileParty fromParty, Settlement toSettlement, ref float __result)
+        {
+            if (toSettlement.StringId == "town_TT1")
+            {
+                Settlement zeonica = MBObjectManager.Instance.GetObject<Settlement>("town_EW2");
+                float zeonicaDistance = Campaign.Current.Models.MapDistanceModel.GetDistance(fromParty, zeonica);
+                Settlement sanala = MBObjectManager.Instance.GetObject<Settlement>("town_A6");
+                float sanalaDistance = Campaign.Current.Models.MapDistanceModel.GetDistance(fromParty, sanala);
+                float avgDistance = (sanalaDistance + zeonicaDistance) / 2f;
+                __result = avgDistance;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DefaultMapDistanceModel), "GetDistance", new[] { typeof(Settlement), typeof(Settlement) })]
+    internal class SettlementLiberartisPatch
+    {
+        public static void Postfix(Settlement fromSettlement, Settlement toSettlement, ref float __result)
+        {
+            if (fromSettlement.StringId == "town_TT1")
+            {
+                Settlement zeonica = MBObjectManager.Instance.GetObject<Settlement>("town_EW2");
+                float zeonicaDistance = Campaign.Current.Models.MapDistanceModel.GetDistance(zeonica, toSettlement);
+                Settlement sanala = MBObjectManager.Instance.GetObject<Settlement>("town_A6");
+                float sanalaDistance = Campaign.Current.Models.MapDistanceModel.GetDistance(sanala, toSettlement);
+                float avgDistance = (sanalaDistance + zeonicaDistance) / 2f;
+                __result = avgDistance;
+            }
+            else if (toSettlement.StringId == "town_TT1")
+            {
+                Settlement zeonica = MBObjectManager.Instance.GetObject<Settlement>("town_EW2");
+                float zeonicaDistance = Campaign.Current.Models.MapDistanceModel.GetDistance(fromSettlement, zeonica);
+                Settlement sanala = MBObjectManager.Instance.GetObject<Settlement>("town_A6");
+                float sanalaDistance = Campaign.Current.Models.MapDistanceModel.GetDistance(fromSettlement, sanala);
+                float avgDistance = (sanalaDistance + zeonicaDistance) / 2f;
+                __result = avgDistance;
+            }
+        }
+    }
+}
--- NavalPatch.cs
+++ NavalPatch.cs
@@ -10,7 +10,6 @@ using SandBox.View.Map;
 using System;
 using System.Reflection;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.Map;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Core;
 using TaleWorlds.Engine;
@@ -18,110 +17,127 @@ using TaleWorlds.Library;
 
 namespace CalradiaExpanded
 {
-  public class NavalPatch
-  {
-    [HarmonyPatch(typeof (MapScene), "GetNavigationMeshIndexOfTerrainType")]
-    public class GetNavigationMeshIndexOfTerrainTypePatch
+    public class NavalPatch
     {
-      public static void Postfix(TerrainType terrainType, int __result)
-      {
-        if (terrainType != TerrainType.Swamp)
-          __result = 9;
-      }
-    }
+        [HarmonyPatch(typeof(MapScene), "GetNavigationMeshIndexOfTerrainType")]
+        public class GetNavigationMeshIndexOfTerrainTypePatch
+        {
+            public static void Postfix(TerrainType terrainType, int __result)
+            {
+                if (terrainType == TerrainType.Swamp && __result != -1)
+                {
+                    __result = 9;
+                }
+            }
+        }
 
-    [HarmonyPatch(typeof (MapScene), "GetFaceTerrainType")]
-    public class GetFaceTerrainTypePatch
-    {
-      public static void Postfix(
-        PathFaceRecord navMeshFace,
-        MapScene __instance,
-        ref TerrainType __result)
-      {
-        if (__instance.Scene.GetIdOfNavMeshFace(((PathFaceRecord)navMeshFace).FaceIndex) == 9)
-            __result = TerrainType.Swamp;
-      }
-    }
+        [HarmonyPatch(typeof(MapScene), "GetFaceTerrainType")]
+        public class GetFaceTerrainTypePatch
+        {
+            public static void Postfix(
+              PathFaceRecord navMeshFace,
+              ref TerrainType __result)
+            {
+                if (navMeshFace.IsValid() && navMeshFace.FaceGroupIndex == 9)
+                {
+                    __result = TerrainType.Swamp;
+                }
+            }
+        }
 
-    [HarmonyPatch(typeof (MobileParty), "TickForMovingMobileParty")]
-    public class OnAiTickInternalPatch
-    {
-      public static MethodInfo SetVisualsMethodInfo = AccessTools.Method(typeof (PartyVisual), "set_MountAgentVisuals", (Type[]) null, (Type[]) null);
-      public static MethodInfo GetBannerOfCharacterMethodInfo = AccessTools.Method(typeof (PartyVisual), "GetBannerOfCharacter", (Type[]) null, (Type[]) null);
+        [HarmonyPatch(typeof(MobileParty), "TickForMovingMobileParty")]
+        public class OnAiTickInternalPatch
+        {
+            public static MethodInfo SetVisualsMethodInfo = AccessTools.Method(typeof(PartyVisual), "set_MountAgentVisuals", (Type[])null, (Type[])null);
+            public static MethodInfo GetBannerOfCharacterMethodInfo = AccessTools.Method(typeof(PartyVisual), "GetBannerOfCharacter", (Type[])null, (Type[])null);
 
-      private static void SetVisualsNull(PartyVisual instance) => SetVisualsMethodInfo.Invoke((object) instance, new object[1]);
+            private static void SetVisualsNull(PartyVisual instance)
+            {
+                SetVisualsMethodInfo.Invoke((object)instance, new object[1]);
+            }
 
-      private static MetaMesh GetBannerOfCharacter(Banner banner, string bannerMeshName) => (MetaMesh) NavalPatch.OnAiTickInternalPatch.GetBannerOfCharacterMethodInfo.Invoke((object) null, new object[2]
-      {
+            private static MetaMesh GetBannerOfCharacter(Banner banner, string bannerMeshName)
+            {
+                return (MetaMesh)NavalPatch.OnAiTickInternalPatch.GetBannerOfCharacterMethodInfo.Invoke((object)null, new object[2]
+            {
         (object) banner,
         (object) bannerMeshName
-      });
+            });
+            }
 
-        public static void Postfix(MobileParty __instance)
-        {
-            try
+            public static void Postfix(MobileParty __instance)
             {
-                if (((PartyVisual)__instance.Party.Visuals).HumanAgentVisuals != null && Campaign.Current.MapSceneWrapper.GetFaceTerrainType(__instance.CurrentNavigationFace) == TerrainType.Swamp && ((PartyVisual)__instance.Party.Visuals).HumanAgentVisuals.GetEntity().GetVisibilityExcludeParents())
+                try
                 {
-                    if (((PartyVisual)__instance.Party.Visuals).StrategicEntity.ChildCount == 0)
+                    PartyVisual visualOfParty = PartyVisualManager.Current.GetVisualOfParty(__instance.Party);
+                    if (visualOfParty.HumanAgentVisuals != null && Campaign.Current.MapSceneWrapper.GetFaceTerrainType(__instance.CurrentNavigationFace) == TerrainType.Swamp && visualOfParty.HumanAgentVisuals.GetEntity().GetVisibilityExcludeParents())
                     {
-                        GameEntity empty = GameEntity.CreateEmpty(((PartyVisual)MobileParty.MainParty.Party.Visuals).StrategicEntity.Scene, true);
-                        if (__instance.IsCaravan)
+                        if (visualOfParty.StrategicEntity.ChildCount == 0)
                         {
-                            MatrixFrame identity = MatrixFrame.Identity;
-                            ((Mat3)identity.rotation).ApplyScaleLocal(1f);
-                            ((MatrixFrame)identity).Rotate(1.4f, Vec3.Up);
-                            empty.AddMultiMesh(MetaMesh.GetCopy("mi_ship_2", true, false), true);
-                            identity.Rotate(1.4f, Vec3.Up);
-                            identity.Scale(new Vec3(1f, 1f, 1f));
-                            empty.SetFrame(ref identity);
-                        }
-                        else
-                        {
-                            MatrixFrame identity = MatrixFrame.Identity;
-                            ((Mat3)identity.rotation).ApplyScaleLocal(0.08f);
-                            if (__instance.Army != null && __instance.Army.LeaderParty != __instance)
-                                ((Mat3)identity.rotation).ApplyScaleLocal(0.8f);
-                            empty.AddMultiMesh(MetaMesh.GetCopy("ship_a", true, false), true);
-                            identity.Scale(new Vec3((float)0.08, (float)0.08, (float)0.08));
-                            empty.SetFrame(ref identity);
-                        }
-                        if (empty.ChildCount == 0)
-                        {
-                            empty.AddChild(GameEntity.CreateEmpty(((PartyVisual)MobileParty.MainParty.Party.Visuals).StrategicEntity.Scene, true), false);
-                            if (__instance.LeaderHero != null && __instance.LeaderHero != null && __instance.LeaderHero.ClanBanner != null)
+                            GameEntity empty = GameEntity.CreateEmpty(visualOfParty.StrategicEntity.Scene, true);
+                            if (__instance.IsCaravan)
                             {
-                                MetaMesh bannerOfCharacter = GetBannerOfCharacter(new Banner(__instance.LeaderHero.ClanBanner.Serialize()), "campaign_flag");
-                                empty.GetChild(0).AddMultiMesh(bannerOfCharacter, true);
                                 MatrixFrame identity = MatrixFrame.Identity;
-                                ((Mat3)identity.rotation).ApplyScaleLocal(4f);
-                                ((MatrixFrame)identity).Advance(-0.21f);
-                                ((MatrixFrame)identity).Strafe(0.03f);
-                                empty.GetChild(0).SetFrame(ref identity);
+                                ((Mat3)identity.rotation).ApplyScaleLocal(1f);
+                                ((MatrixFrame)identity).Rotate(1.4f, Vec3.Up);
+                                empty.AddMultiMesh(MetaMesh.GetCopy("mi_ship_2", true, false), true);
+                                identity.Rotate(1.4f, Vec3.Up);
+                                identity.Scale(new Vec3(1f, 1f, 1f));
+                                empty.SetFrame(ref identity);
+                            }
+                            else
+                            {
+                                MatrixFrame identity = MatrixFrame.Identity;
+                                ((Mat3)identity.rotation).ApplyScaleLocal(0.08f);
+                                if (__instance.Army != null && __instance.Army.LeaderParty != __instance)
+                                {
+                                    ((Mat3)identity.rotation).ApplyScaleLocal(0.8f);
+                                }
+
+                                empty.AddMultiMesh(MetaMesh.GetCopy("ship_a", true, false), true);
+                                identity.Scale(new Vec3((float)0.08, (float)0.08, (float)0.08));
+                                empty.SetFrame(ref identity);
+                            }
+                            if (empty.ChildCount == 0)
+                            {
+                                empty.AddChild(GameEntity.CreateEmpty(PartyVisualManager.Current.GetVisualOfParty(MobileParty.MainParty.Party).StrategicEntity.Scene, true), false);
+                                if (__instance.LeaderHero != null && __instance.LeaderHero != null && __instance.LeaderHero.ClanBanner != null)
+                                {
+                                    MetaMesh bannerOfCharacter = GetBannerOfCharacter(new Banner(__instance.LeaderHero.ClanBanner.Serialize()), "campaign_flag");
+                                    empty.GetChild(0).AddMultiMesh(bannerOfCharacter, true);
+                                    MatrixFrame identity = MatrixFrame.Identity;
+                                    ((Mat3)identity.rotation).ApplyScaleLocal(4f);
+                                    ((MatrixFrame)identity).Advance(-0.21f);
+                                    ((MatrixFrame)identity).Strafe(0.03f);
+                                    empty.GetChild(0).SetFrame(ref identity);
+                                }
+                            }
+                            visualOfParty.StrategicEntity.AddChild(empty, false);
+                        }
+                        if (visualOfParty.HumanAgentVisuals.GetEntity().GetVisibilityExcludeParents())
+                        {
+                            visualOfParty.HumanAgentVisuals.GetEntity().SetVisibilityExcludeParents(false);
+                            if (visualOfParty.MountAgentVisuals != null)
+                            {
+                                visualOfParty.MountAgentVisuals.Reset();
+                                NavalPatch.OnAiTickInternalPatch.SetVisualsNull(visualOfParty);
+                            }
+                            if (visualOfParty.CaravanMountAgentVisuals != null)
+                            {
+                                visualOfParty.CaravanMountAgentVisuals.GetEntity().SetVisibilityExcludeParents(false);
                             }
                         }
-                        ((PartyVisual)__instance.Party.Visuals).StrategicEntity.AddChild(empty, false);
                     }
-                    if (((PartyVisual)__instance.Party.Visuals).HumanAgentVisuals.GetEntity().GetVisibilityExcludeParents())
+                    if (Campaign.Current.MapSceneWrapper.GetFaceTerrainType(__instance.CurrentNavigationFace) != TerrainType.Swamp && visualOfParty.StrategicEntity.ChildCount > 0)
                     {
-                        ((PartyVisual)__instance.Party.Visuals).HumanAgentVisuals.GetEntity().SetVisibilityExcludeParents(false);
-                        if (((PartyVisual)__instance.Party.Visuals).MountAgentVisuals != null)
-                        {
-                            ((PartyVisual)__instance.Party.Visuals).MountAgentVisuals.Reset();
-                            NavalPatch.OnAiTickInternalPatch.SetVisualsNull((PartyVisual)__instance.Party.Visuals);
-                        }
-                        if (((PartyVisual)__instance.Party.Visuals).CaravanMountAgentVisuals != null)
-                            ((PartyVisual)__instance.Party.Visuals).CaravanMountAgentVisuals.GetEntity().SetVisibilityExcludeParents(false);
+                        __instance.Party.SetVisualAsDirty();
                     }
                 }
-                if (Campaign.Current.MapSceneWrapper.GetFaceTerrainType(__instance.CurrentNavigationFace) != TerrainType.Swamp && ((PartyVisual)__instance.Party.Visuals).StrategicEntity.ChildCount > 0)
-                    __instance.Party.Visuals.SetMapIconAsDirty();
-            }
-            catch (Exception ex)
-            {
-                MBDebug.ShowWarning(string.Format("Error while trying to show party as ship. Party: [{0}]\nException: [{1}]", (object)__instance.Name, (object)ex.Message));
+                catch (Exception ex)
+                {
+                    MBDebug.ShowWarning(string.Format("Error while trying to show party as ship. Party: [{0}]\nException: [{1}]", (object)__instance.Name, (object)ex.Message));
+                }
             }
         }
     }
-  }
 }
